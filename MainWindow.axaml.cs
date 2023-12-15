5bc68239207453c888c872dcb31ebcfafbf2b4fa
PolinaGabriel <polina_gabriel@live.ru>
2023-12-15 10:20:59 +0300

    first commit

---
+++ MainWindow.axaml.cs
@@ -0,0 +1,19 @@
+using Avalonia.Controls;
+using System;
+using System.IO;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Avalonia;
+using Avalonia.Media.Imaging;
+using Avalonia.Platform;
+
+namespace AvaloniaApplication1
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
